20122e500fc6bb27b736489d08b487f45abe0bba
trqhuy <145922644+trqhuy@users.noreply.github.com>
2024-11-18 01:34:42 +0700

    commit

---
+++ cpp/Customer.cpp
@@ -0,0 +1,23 @@
+#include "../header/Customer.h"
+
+
+using namespace std;
+Customer::Customer(string soThe, string tenChuThe, string pin, int soDu)
+	: soThe(soThe), tenChuThe(tenChuThe), PIN(pin), soDu(soDu) {}
+
+string Customer::getSoThe() {
+	return soThe;
+}
+
+string Customer::getTenChuThe() {
+	return tenChuThe;
+}
+
+string Customer::getPin() {
+	return PIN;
+}
+
+int Customer::getSoDu() {
+	return soDu;
+}
+
